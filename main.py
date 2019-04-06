b088b58dd938e948a91994080e36e5c3735ff83b
Brett Largent <largentba21@gmail.com>
2019-04-06 15:31:37 -0500

    working Dijkstra's with print statements

--- main.py
+++ main.py
@@ -26,23 +26,29 @@ with open(fname) as inFile:
 
     for key in d:
         print(key, ": ", d[key])
-
+print('\n')
 
 #startVertex = input("Please provide a start vertex label (1..n):\n")
 startVertex = 1
 visited = {startVertex}
 pathLengths = [0] * len(d)
 
-#while len(visited) != len(d):
-iteration = []
-for v in visited:
-    for i in range(len(d[v])):
-        if d[v][i][0] not in visited:
-            iteration.append((v, d[v][i]))
-minEdge = min(iteration, key = lambda t: t[1][1])
-print(iteration)
-print("Minimum edge is:", minEdge)
-visited.add(minEdge[1][0])
-pathLengths[minEdge[1][0]-1] = pathLengths[minEdge[0]-1] + minEdge[1][1]
-print(visited)
-print(pathLengths)
+while len(visited) != len(d):
+    iteration = []
+    for v in visited:
+        for i in range(len(d[v])):
+            if d[v][i][0] not in visited:
+                iteration.append([v, d[v][i][0], d[v][i][1]])
+                iteration[len(iteration)-1][2] += pathLengths[iteration[len(iteration)-1][0]-1]
+                print("Path_Lengths:", pathLengths)
+                print("Iteration:", iteration)
+                print("lvw:", iteration[len(iteration)-1][2])
+                print("path_lengths[v]:", pathLengths[iteration[len(iteration)-1][0]-1])
+    minEdge = min(iteration, key = lambda t: t[2])
+    print("Considers:", iteration)
+    print("Minimum edge is:", minEdge)
+    visited.add(minEdge[1])
+    pathLengths[minEdge[1]-1] = minEdge[2]
+    print("Path_Lengths:", pathLengths, '\n')
+
+print('\n',pathLengths)
